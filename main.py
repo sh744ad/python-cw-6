60c87006995622b061a6755d45bc520aff0e5d5c
sh744ad <142683909+sh744ad@users.noreply.github.com>
2023-09-12 22:23:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-# write your code here
+person ={
+    "name" : "shahad",
+    "age" :18,
+    "hobbies": ["painting","reading"]
+
+}
+print(person["name"])
+print(person["age"])
+person["age"]=19
+person["country"]= "kuwait"
+print(person)
+print(f"number of objects in dictionary: {len((person))}")
+person["hobbies"].append("coding")
+
+def check_hobbies(person):
+    if len(person["hobbies"]) >=4:
+        return print("wow you are amzing")
+    else:
+        return print("keep exploring your hobbies")
+check_hobbies(person)   
